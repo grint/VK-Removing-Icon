bc2d938bc096944c753096731df36bbeaac4f414
abwr <grint@inbox.ru>
2017-04-21 01:02:06 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# VK-Removing-Icon
-Add additional removing icon for posts in VKontakte outside of dropdown menu
+# VKontakte Removing Icon
+
+After redesigning of VKontakte the removing icon for posts was placed to the post's dropdown menu, which makes it not comfortable to hide each readed post. The user-script for Tampermonkey adds additional removing icon for all posts outside of dropdown menu, so now there's no need of additional actions.
+
+![](https://cloud.githubusercontent.com/assets/1681601/25255890/9afe891e-262d-11e7-914b-c507ba74f575.png)
---
+++ VK-Removing-Icon.user.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name         VK Removing Icon
+// @include      https://vk.com/feed
+// @version      0.1
+// @description  Add additional removing icon for posts outside of dropdown menu
+// @author       Anastasia Grineva
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var css = '.remove-post-icon {position: absolute; right: 13px; top: 24px; font-size: 13px;} .ui_actions_menu_icons {right: 40px !important;}',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+
+    // Add CSS for removing icon
+    if (style.styleSheet){
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+    style.type = 'text/css';
+    head.appendChild(style);
+    
+    // Update posts by page load
+    updatePosts();
+    
+    // Observe DOM change (adding new posts on scroll) and update new posts
+    var numOfChanges = 0;
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList') {
+                numOfChanges++;
+            }
+            if(numOfChanges >= 10) {
+                numOfChanges = 0;
+                updatePosts();
+            }
+        });
+    });
+    observer.observe(document.getElementById('feed_rows'), { childList: true });
+    
+    // Update posts by adding them on "Load new posts" button click
+    document.querySelector('.feed_new_posts').onclick = function(){
+        updatePosts();
+    };
+    
+    // Update posts by clicking on "News" button in the right menu
+    document.getElementById('ui_rmenu_news').onclick = function(){
+        updatePosts();
+    };
+
+    // Function for inserting icon to a post
+    function addIcon(post) {
+        if(post) {
+            post.setAttribute("class", post.className + " with-icon");
+            var originalRemoveLink = post.querySelector("a[onclick^='feed.ignoreItem']");
+            if(originalRemoveLink) {
+                var onclickAction = originalRemoveLink.getAttribute("onclick");
+                var link = document.createElement("a");
+                link.innerHTML = "&#10005;"; // cross sign
+                link.setAttribute("onclick", onclickAction);
+                link.setAttribute("class", "remove-post-icon");
+                post.querySelector(".post_header_info").appendChild(link);
+            }
+        }
+    }
+
+    // Function for processing posts,
+    // adds removing icon and special class to each non-updated post
+    function updatePosts() {
+        var posts = document.getElementsByClassName('feed_row');
+        for (var i = 0; i < posts.length; i++) {
+            if(!hasClass(posts[i], "with-icon")) {
+                addIcon(posts[i]);
+            }
+        }
+    }
+    
+    function hasClass(element, cls) {
+        return (" " + element.className + " ").indexOf(" " + cls + " ") > -1;
+    }
+})();
+
